d75fc5138325ca3d01e0fbc2abb113212926e0ae
Bogumil.Harnik <Bogumil.Harnik@BHARNIK-25>
2023-04-26 17:29:36 +0200

    Dodaj pliki projektów.

---
+++ Kurier/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurier
+{
+    public class Kurier
+    {
+        public static void Main(string[] args)
+        {
+            //enum UnitOfMeasure
+            //    {
+            //        milimetry,
+            //        centymerty,
+            //        metry
+            //    }
+         
+             System.Console.WriteLine("Cos");
+
+        }
+    }
+}
---
+++ Kurier/Pudelko.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurier
+{
+    public class Pudelko
+    {
+        private int dlugosc;
+        private int szerokosc;
+        private int wysokosc;
+        private string jednostki;
+
+
+     
+
+    // tworzenie pudelka - konstruktor
+    public Pudelko(int dl, int sz, int wy, string j="cm")
+        {
+            this.dlugosc = dl;
+            this.szerokosc = sz;
+            this.wysokosc = wy;
+            this.jednostki = j;
+
+
+            if (dl < 0 || sz < 0 || wy < 0)
+                {
+
+                Console.WriteLine("Wymiary pudełka nie mogą być mniejsze niż 0");
+
+                }
+                
+
+            
+
+        }
+    }
+}
